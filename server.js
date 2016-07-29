0d58252c3e22c302d508b6e2aa0c7aeba4c5e67f
Raja Sekar <rajasekar89be@gmail.com>
2016-07-29 23:37:19 +0530

    heroku issue fix

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 var express = require('express');
-var pagerouter = require('./app/router/page.router');
-var config = require('./app/config/config');
-
 var app = express();
-
-// app.use(express.static(__dirname + '/public'));
-app.use('/', pagerouter);
+var port = process.env.PORT || 2000;
 
 
-app.listen(config.port, function(err){
-  if(err) {
-    console.log("Error in connecting..");
-  }
-  console.log("App connected to "+config.port);
+app.get('/api/check', function(req, res){
+  res.json({status: true, message: "Hello world!!"})
 });
+
+app.listen(port);
+console.log("App listening on port "+ port);
